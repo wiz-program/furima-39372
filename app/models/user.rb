742ac8c601e984076db4741547c930a3ced18d6b
wiz-program <ywizmida@outlook.jp>
2023-09-11 17:23:03 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/
+         VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
          KATAKANA_REGEX = /\A[ｧ-ﾝﾞﾟァ-ヶー－]+\z/
 
 
          validates :password, format: { with: VALID_PASSWORD_REGEX }
-         validates :password_confirmation, presence: true
+         
          validates :nickname, presence: true
          validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
          validates :first_name, presence: true,format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
