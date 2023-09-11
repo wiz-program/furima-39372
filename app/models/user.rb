7c920c4acf883018fbe8febb11b337c61bb87e7f
wiz-program <ywizmida@outlook.jp>
2023-09-11 15:06:20 +0900

    ユーザーの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/
          KATAKANA_REGEX = /\A[ｧ-ﾝﾞﾟァ-ヶー－]+\z/
 
 
-         validates :password, format: { with: /\A[a-z0-9]+\z/i }
+         validates :password, format: { with: VALID_PASSWORD_REGEX }
+         validates :password_confirmation, presence: true
          validates :nickname, presence: true
          validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
          validates :first_name, presence: true,format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
