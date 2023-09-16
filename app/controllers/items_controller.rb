ba7410375b47da74f0db452a774484a5b9c23dc8
wiz-program <ywizmida@outlook.jp>
2023-09-16 17:57:13 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,9 +20,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  #   @item = Item.find(params[:id])
-  # end
+  def show
+    @item = Item.find(params[:id])
+  end
 
   # def edit
     # @item = Item.find(params[:id])
