e5d1228700252b147c40b219552064012e0bf7e7
wiz-program <ywizmida@outlook.jp>
2026-06-28 14:34:58 +0900

    Renderに公開するために、loggerの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,10 @@
 # exit on error
 set -o errexit
 
+export RAILS_ENV=production
+
 bundle install
 yarn install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate RAILS_ENV=production
+bundle exec rake db:migrate
--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'logger' # Rails 6.1 + concurrent-ruby 1.3.5+ で Logger が未ロードになる問題の回避
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
